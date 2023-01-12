141f1c972823d07439ebd316d111ef7b5623597a
ZagrosJaff <zagros.jaff@hotmail.co.uk>
2023-01-12 21:16:39 +0000

    test commit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,3 @@
 class PagesController < ApplicationController
-  def home
-    puts "Hi Hi Test"
-  end
+
 end
--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,6 +1,3 @@
 module PagesHelper
 
-  def test_helper
-    puts "hi test"
-  end
 end
