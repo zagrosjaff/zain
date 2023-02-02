bb0e460f63ba8dec132dd5ed8ca8c3d935c9c03f
ZagrosJaff <zagros.jaff@hotmail.co.uk>
2023-02-02 18:54:47 +0000

    no message

--- db/migrate/20230127000003_add_timestamps_to_articles.rb
+++ db/migrate/20230127000003_add_timestamps_to_articles.rb
@@ -1,8 +1,8 @@
 class AddTimestampsToArticles < ActiveRecord::Migration[6.1]
-  def change
-
-    add_column :articles, :created_at, :datetime
-    add_column :articles, :updated_st, :datetime
-
-  end
+  # def change
+  #
+  #   add_column :articles, :created_at, :datetime
+  #   add_column :articles, :updated_st, :datetime
+  #
+  # end
 end
